b02caa9ad3b5991a07f37149e7161dd647f60909
Danny Massa <dan.massa@outlook.com>
2021-10-28 09:16:35 -0500

    Adding three levels of log verbosity

--- services/log_service.go
+++ services/log_service.go
@@ -3,38 +3,71 @@ package services
 import (
 	"errors"
 	"fmt"
-	"github.com/DannyMassa/dead-link-linter/types"
 	"sort"
 	"strings"
+
+	"github.com/DannyMassa/dead-link-linter/types"
 )
 
 type LogService interface {
-	PrintResults(directory string, fileEndings []string) []string
+	PrintResults(results []*types.URL, config *types.Config) error
 }
 
 type LogServiceImpl struct{}
 
 func (l LogServiceImpl) PrintResults(results []*types.URL, config *types.Config) error {
 	successCount, failCount, skipCount := 0, 0, 0
-	tmpUrls := results
-	sort.SliceStable(tmpUrls, func(i, j int) bool {
+	tmpResults := results
+
+	// Get results summary before manipulating results for output
+	for _, result := range tmpResults {
+		if result.Result == "SUCCESS" {
+			successCount++
+		}
+		if result.Result == "FAILURE" {
+			failCount++
+		}
+		if result.Result == "SKIPPED" {
+			skipCount++
+		}
+	}
+
+	// Remove objects not going to be used in output
+	if config.LogVerbosity < 2 {
+		if config.LogVerbosity < 1 {
+			for i := len(tmpResults) - 1; i >= 0; i-- {
+				if tmpResults[i].Result == "SUCCESS" || tmpResults[i].Result == "SKIPPED" {
+					tmpResults = append(tmpResults[:i], tmpResults[i+1:]...)
+				}
+			}
+		} else {
+			for i := len(tmpResults) - 1; i >= 0; i-- {
+				if tmpResults[i].Result == "SUCCESS" {
+					tmpResults = append(tmpResults[:i], tmpResults[i+1:]...)
+				}
+			}
+		}
+	}
+
+	// Sort Output
+	sort.SliceStable(tmpResults, func(i, j int) bool {
 		less := false
 		// Sort by Directory
-		if strings.EqualFold(tmpUrls[i].Directory, tmpUrls[j].Directory) { //nolint
+		if strings.EqualFold(tmpResults[i].Directory, tmpResults[j].Directory) { //nolint
 			// If Directory == Directory, sort by File
-			if strings.EqualFold(tmpUrls[i].File, tmpUrls[j].File) { //nolint
+			if strings.EqualFold(tmpResults[i].File, tmpResults[j].File) { //nolint
 				// If Directory == Directory && File == File, sort alphabetically
-				if strings.ToLower(tmpUrls[i].Link) < strings.ToLower(tmpUrls[j].Link) {
+				if strings.ToLower(tmpResults[i].Link) < strings.ToLower(tmpResults[j].Link) {
 					less = true
 				} else {
 					less = false
 				}
-			} else if strings.ToLower(tmpUrls[i].File) < strings.ToLower(tmpUrls[j].File) {
+			} else if strings.ToLower(tmpResults[i].File) < strings.ToLower(tmpResults[j].File) {
 				less = true
 			} else {
 				less = false
 			}
-		} else if strings.ToLower(tmpUrls[i].Directory) < strings.ToLower(tmpUrls[j].Directory) {
+		} else if strings.ToLower(tmpResults[i].Directory) < strings.ToLower(tmpResults[j].Directory) {
 			less = true
 		} else {
 			less = false
@@ -43,27 +76,20 @@ func (l LogServiceImpl) PrintResults(results []*types.URL, config *types.Config)
 		return less
 	})
 
-	if config.LogVerbosity <= 1 {
-		for i := len(tmpUrls) - 1; i >= 0; i-- {
-			if tmpUrls[i].Result == "SUCCESS" {
-				tmpUrls = append(tmpUrls[:i], tmpUrls[i+1:]...)
-			}
-		}
-	}
-
-	for i := 0; i < len(tmpUrls); i++ {
+	// Print Output
+	for i := 0; i < len(tmpResults); i++ {
 		if i == 0 {
-			fmt.Printf("%s\n", tmpUrls[i].Directory)
-			fmt.Printf("    %s\n", tmpUrls[i].File)
-			fmt.Printf("        %s - %s\n", tmpUrls[i].Result, tmpUrls[i].Link)
+			fmt.Printf("%s\n", tmpResults[i].Directory)
+			fmt.Printf("    %s\n", tmpResults[i].File)
+			fmt.Printf("        %s - %s\n", tmpResults[i].Result, tmpResults[i].Link)
 		} else {
-			if tmpUrls[i].Directory != tmpUrls[i-1].Directory {
-				fmt.Printf("%s\n", tmpUrls[i].Directory)
+			if tmpResults[i].Directory != tmpResults[i-1].Directory {
+				fmt.Printf("%s\n", tmpResults[i].Directory)
 			}
-			if tmpUrls[i].File != tmpUrls[i-1].File {
-				fmt.Printf("    %s\n", tmpUrls[i].File)
+			if tmpResults[i].File != tmpResults[i-1].File {
+				fmt.Printf("    %s\n", tmpResults[i].File)
 			}
-			fmt.Printf("        %s - %s\n", tmpUrls[i].Result, tmpUrls[i].Link)
+			fmt.Printf("        %s - %s\n", tmpResults[i].Result, tmpResults[i].Link)
 		}
 	}
 
